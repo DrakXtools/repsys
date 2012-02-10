39f1a32d93adef18e47ac9cb432d655bda60bb96
Nicolas Vigier <boklm@mageia.org>
2012-02-10 21:14:23 +0000

    add sql file to create youridb

---
+++ youridb.sql
@@ -0,0 +1,1 @@
+CREATE TABLE submits ( sha1 character(40), rpmname varchar(512), EVR varchar(100), packager varchar(100), url varchar(512), revision integer, status integer, submit_ts timestamp with time zone );
