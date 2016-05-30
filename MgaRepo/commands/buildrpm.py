1d80697535027f5d9307616b7d727b72a429b538
Per Øyvind Karlsen <proyvind@moondrake.org>
2016-05-30 17:56:12 +0200

    document buildrpm options

--- MgaRepo/commands/buildrpm.py
+++ MgaRepo/commands/buildrpm.py
@@ -11,13 +11,16 @@ Builds the binary RPM(s) (.rpm) file(s) of a given package.
 
 Options:
     -l         Disable rpmlint check of packages built
+    -P USER    Define the RPM packager information to USER
+    -q         Silent build output
+
 """
 
 def parse_options():
     parser = OptionParser(HELP)
     parser.add_option("-b", dest="build_cmd", default="a")
     parser.add_option("-P", dest="packager", default="")
-    parser.add_option("-v", "--verbose", dest="verbose", action="store_true", default=False)
+    parser.add_option("-q", "--quiet", dest="verbose", action="store_false", default=True)
     parser.add_option("-l", dest="rpmlint", action="store_false", default=True)
     opts, args = parser.parse_args()
     return opts
